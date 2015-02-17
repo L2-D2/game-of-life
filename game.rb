e325dde7a978259b4a58620e935e4b08f646736e
Jonan Scheffler <jonanscheffler@gmail.com>
2015-02-16 21:34:14 -0800

    Width and height instead of size.

--- game.rb
+++ game.rb
@@ -3,36 +3,35 @@ require './world'
 
 class Game
   attr_accessor :world
-  attr_reader :world_size
 
-  def initialize
-    @world_size = 10
-    @world = World.new(self.world_size)
+  def initialize(width:, height:)
+    @world = World.new(width, height)
   end
-  
+
   def tick
     # traverse map and check surrounding cell state
     whole_new_world = self.world.clone
-    
+
     world.grid.each_with_index do |row, row_number|
       row.each_with_index do |cell, column_number|
-        neighbors = world.get_neighbors(x: row_number, y: column_number)
-      
+        neighbors = world.get_neighbors(y: row_number, x: column_number)
+
         # store determined value in new world
-        
-      if can_i_be_alive_next_time_please?(neighbors, cell.lifeyness)
-        whole_new_world.grid[row_number][column_number].live!
-      else
-        whole_new_world.grid[row_number][column_number].die!
+
+        if can_i_be_alive_next_time_please?(neighbors, cell.lifeyness)
+          whole_new_world.grid[row_number][column_number].live!
+        else
+          whole_new_world.grid[row_number][column_number].die!
+        end
       end
-    end
-   
-    # set new world to old world
-    self.world = whole_new_world
-    
-    # draw
-      
-      `clear`
+
+      # set new world to old world
+      self.world = whole_new_world
+
+      # draw
+
+      system "clear"
+
       world.grid.each do |row|
         row.each do |cell|
           if cell.lifeyness
@@ -41,15 +40,15 @@ class Game
             print(" ")
           end
         end
+
         puts
       end
-      puts
     end
   end
-  
+
   def can_i_be_alive_next_time_please?(neighbors, alive)
     count = neighbors.select(&:alive?).length
-    
+
     if alive
       if count == 2 || count == 3
         return true
@@ -63,13 +62,13 @@ class Game
         return false
       end
     end
-    
+
   end
 end
 
-game = Game.new
+game = Game.new(width: 50, height: 10)
 
 while true do
   game.tick
-  sleep 1
+  sleep 0.1
 end
--- world.rb
+++ world.rb
@@ -1,23 +1,29 @@
 class World
   attr_reader :grid
-  
-  def initialize(size)
-    @grid = Array.new(size) { Array.new(size) { Cell.new } }
+
+  def initialize(width, height)
+    @grid = Array.new(height) { Array.new(width) { Cell.new } }
   end
-  
+
   def get_neighbors(x:, y:)
-    max = self.grid.length - 1
+    max_y = self.grid.length - 2
+    max_x = self.grid.first.length - 2
+
     [
-      (self.grid[x][y+1] if y < max),
-      (self.grid[x+1][y] if x < max),
-      (self.grid[x-1][y] if x > 0),
-      (self.grid[x][y-1] if y > 0),
-      
-      (self.grid[x+1][y-1] if x < max && y > 0),
-      (self.grid[x+1][y+1] if x < max && y < max),
-      (self.grid[x-1][y+1] if x > 0   && y < max),
-      (self.grid[x-1][y-1] if x > 0   && y > 0)
+      (self.grid[y][x+1] if x < max_x),
+      (self.grid[y+1][x] if y < max_y),
+      (self.grid[y-1][x] if y > 0),
+      (self.grid[y][x-1] if y > 0),
+
+      (self.grid[y+1][x-1] if y < max_y && x > 0),
+      (self.grid[y+1][x+1] if y < max_y && x < max_x),
+      (self.grid[y-1][x+1] if y > 0     && x < max_x),
+      (self.grid[y-1][x-1] if y > 0     && x > 0)
     ].compact
+  rescue => e
+    $debug = true
+    require 'pry'; binding.pry if $debug
+    $debug = false
   end
-  
+
 end
